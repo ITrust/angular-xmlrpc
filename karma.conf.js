f84294eb86d634d819320bfe5e0cc75e53ded070
Julien Chakra <julien.chakra@gmail.com>
2016-09-09 16:43:13 +0200

    Add uglify script and file. Create a src and dist folders.

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
     files: [
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'xmlrpc.js',
+      'src/xmlrpc.js',
       'test/**/*.js'
     ],
 
